d744ff5ba4acc60b7499f6b849e9d07759d6e9b5
mattany <mattany@gmail.com>
2021-01-23 04:33:27 +0200

    started state generator

--- Markov_Chain.py
+++ Markov_Chain.py
@@ -1,4 +1,5 @@
-from typing import List, NamedTuple
+import copy
+from typing import List, NamedTuple, Tuple
 import numpy as np
 
 
@@ -13,8 +14,11 @@ class Transition(NamedTuple):
     sink_id: int
     probability: float
 
+class Event(NamedTuple):
+    sign: str
+    probability: float
 
-class MDP(object):
+class Markov_Chain(object):
     def __init__(self, states: List[State], transitions: List[Transition]):
         self.state_amount = len(states)
         for i, state in enumerate(states):
@@ -30,7 +34,7 @@ class MDP(object):
         self.construct_vector()
 
     def construct_matrix(self):
-        self.transition_matrix = np.zeros([self.state_amount,self.state_amount], dtype=float)
+        self.transition_matrix = np.zeros([self.state_amount, self.state_amount], dtype=float)
         for t in self.transitions:
             assert 0 <= t.source_id < self.state_amount and 0 <= t.sink_id < self.state_amount
             self.transition_matrix[t.source_id, t.sink_id] = t.probability
@@ -56,7 +60,7 @@ class MDP(object):
                 src = s
             elif s.name == dest_state:
                 dest = s
-        assert(src and dest)
+        assert (src and dest)
         linear_equation = np.copy(self.transition_matrix)
         for i, row in enumerate(linear_equation):
             if i != dest.id:
@@ -67,14 +71,24 @@ class MDP(object):
         solution_vector = np.dot(inverse, b_vector)
         return solution_vector[src.id]
 
-
     def __repr__(self):
         out_str = f"After {self.steps} steps.\n"
         for i, prob in enumerate(self.state_vector):
             out_str += f"State:{self.states[i].name}, Probability: {prob}\n"
         return out_str
+
     def show_transition_matrix(self):
         print(self.transition_matrix)
 
 
-
+def state_generator(goal_event_sequence: str,  events: Tuple[Event]):
+    assert sum(e.probability for e in events) == 1
+    stack = [State("", 0, 1)]
+    ret_val = list()
+    i, id = 0, 1
+    while stack:
+        cur = stack.pop()
+        ret_val.append(cur)
+        for e in events:
+            if goal_event_sequence[i] == e.sign:
+                stack.append(State(goal_event_sequence[:i + 1], id, ))
--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 import numpy as np
-from MDP import *
-
-# def func(n):
-#     x = symbols('x')
-#     if n == 6:
-#         return 0
-#     if n == 1:
-#         expr = x - (1 + (func(2) + x) / 2)
-#         print(expr)
-#
-#         return solve(expr)
-#     expr = 1 + (x + (func(n + 1))) / 2
-#     print(expr, n)
-#     return expr
+from Markov_Chain import *
 
 
 states = [State("", 0, 1),
@@ -32,7 +19,7 @@ transitions = [Transition(0, 1, 0.4),
                Transition(3, 0, 0.6),
                Transition(4, 4, 1)]
 
-markov_process = MDP(states, transitions)
+markov_process = Markov_Chain(states, transitions)
 print(markov_process)
 markov_process.show_transition_matrix()
 markov_process.progress_k_steps(50)
@@ -41,6 +28,35 @@ markov_process.reset()
 print(markov_process)
 print(markov_process.expected_number_of_steps_between_states("", "ABBA"))
 
+# Coin Flips
+states = [State("", 0, 1),
+          State("1", 1, 0),
+          State("11", 2, 0),
+          State("111", 3, 0),
+          State("1111", 4, 0),
+          State("11111", 5, 0)
+          ]
+
+transitions = [Transition(0, 1, 0.5),
+               Transition(0, 0, 0.5),
+               Transition(1, 2, 0.5),
+               Transition(1, 0, 0.5),
+               Transition(2, 3, 0.5),
+               Transition(2, 0, 0.5),
+               Transition(3, 4, 0.5),
+               Transition(3, 0, 0.5),
+               Transition(4, 5, 0.5),
+               Transition(4, 0, 0.5),
+               Transition(5, 5, 1)]
+
+markov_process = Markov_Chain(states, transitions)
+print(markov_process)
+markov_process.show_transition_matrix()
+markov_process.progress_k_steps(50)
+print(markov_process)
+markov_process.reset()
+print(markov_process)
+print(markov_process.expected_number_of_steps_between_states("", "11111"))
 
 # state_vector = np.array([0] * 6)
 # state_vector[0] = 1
