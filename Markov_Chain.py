0e6801c34b122d60171ec358a45e330ba9292b28
mattany <mattany@gmail.com>
2021-01-23 19:44:25 +0200

    done

--- Markov_Chain.py
+++ Markov_Chain.py
@@ -1,24 +1,30 @@
 import copy
 from typing import List, NamedTuple, Tuple
 import numpy as np
+import networkx as nx
+from graphviz import Source
+
 
 
 class State(NamedTuple):
     name: str
     id: int
-    initial_probability: float
+    initial_probability: float = 0
 
 
 class Transition(NamedTuple):
-    source_id: int
-    sink_id: int
+    origin: State
+    destination: State
     probability: float
+
 
 class Event(NamedTuple):
     sign: str
     probability: float
 
+
 class Markov_Chain(object):
+
     def __init__(self, states: List[State], transitions: List[Transition]):
         self.state_amount = len(states)
         for i, state in enumerate(states):
@@ -36,8 +42,8 @@ class Markov_Chain(object):
     def construct_matrix(self):
         self.transition_matrix = np.zeros([self.state_amount, self.state_amount], dtype=float)
         for t in self.transitions:
-            assert 0 <= t.source_id < self.state_amount and 0 <= t.sink_id < self.state_amount
-            self.transition_matrix[t.source_id, t.sink_id] = t.probability
+            assert 0 <= t.origin.id < self.state_amount and 0 <= t.destination.id < self.state_amount
+            self.transition_matrix[t.origin.id, t.destination.id] = t.probability
         assert (sum(t) == 1 for t in self.transition_matrix)
 
     def construct_vector(self):
@@ -53,14 +59,19 @@ class Markov_Chain(object):
             self.state_vector = np.dot(self.state_vector, self.transition_matrix)
         self.steps += k
 
-    def expected_number_of_steps_between_states(self, source_state: str, dest_state: str):
+    def get_states(self):
+        return self.states
+
+    def get_transitions(self):
+        return self.transitions
+
+    def get_expected_number_of_steps_to_goal(self, source_state: str) -> float:
         src, dest = None, None
         for s in self.states:
             if s.name == source_state:
                 src = s
-            elif s.name == dest_state:
-                dest = s
-        assert (src and dest)
+        assert src
+        dest = self.states[-1]
         linear_equation = np.copy(self.transition_matrix)
         for i, row in enumerate(linear_equation):
             if i != dest.id:
@@ -80,15 +91,63 @@ class Markov_Chain(object):
     def show_transition_matrix(self):
         print(self.transition_matrix)
 
+    def draw_transitions(self):
+        G = nx.MultiDiGraph()
+        edges = {}
+        for t in self.transitions:
+            edges[(t.origin.name, t.destination.name)] = t.probability
+        states = [s.name for s in self.states]
+        G.add_nodes_from(states)
+        for k, v in edges.items():
+            origin, destination = k[0], k[1]
+            G.add_edge(origin, destination, weight=v, label=v)
+        pos = nx.drawing.nx_pydot.graphviz_layout(G, prog='dot')
+        nx.draw_networkx(G, pos)
+        nx.drawing.nx_pydot.write_dot(G, 'Chain.dot')
+        Source.from_file('Chain.dot').view()
 
-def state_generator(goal_event_sequence: str,  events: Tuple[Event]):
-    assert sum(e.probability for e in events) == 1
-    stack = [State("", 0, 1)]
-    ret_val = list()
-    i, id = 0, 1
-    while stack:
-        cur = stack.pop()
-        ret_val.append(cur)
-        for e in events:
-            if goal_event_sequence[i] == e.sign:
-                stack.append(State(goal_event_sequence[:i + 1], id, ))
+    @classmethod
+    def get_chain_from_events(cls, terminals: [str], events: List[Event], starting_states: List[State] = None):
+        assert sum(e.probability for e in events) == 1
+        state_dictionary = {t[:i]: 0 for t in terminals for i in range(1, len(t) + 1)}
+        state_dictionary["_"] = 0
+        transitions = list()
+        if not starting_states:
+            starting_states = ["_"]
+        length = len(starting_states)
+        for state_name in starting_states:
+            state_dictionary[state_name] = 1 / length
+        states = list()
+        for i, (k, v) in enumerate(state_dictionary.items()):
+            if i < len(state_dictionary.items()) - 1:
+                states.append(State(k, i + 1, v))
+        empty_state_probability = next(v for k,v in state_dictionary.items() if k == "_" )
+        states.append(State("_", 0, empty_state_probability))
+        # states = [State(k, ,v) if k != "_" else State(k, 0, v) for k, v in state_dictionary.items()]
+        # id = 0
+        for state in states:
+            inital_state_probability = 0
+            if state.name not in terminals:
+                for event in events:
+                    next_state = states[-1]
+                    new_state_name = ""
+                    appended = state.name + event.sign
+                    state_names = [_.name for _ in states]
+                    done = False
+                    for i in range(len(appended)):
+                        if appended[i:] in state_names:
+                            new_state_name = appended[i:]
+                            break
+                    if new_state_name == "":
+                        inital_state_probability += event.probability
+                    else:
+                        next_state = next(_ for _ in states if _.name == new_state_name)
+                        if state.name == "":
+                            state = states[-1]
+                        transitions.append(Transition(state, next_state, event.probability))
+                if inital_state_probability > 0:
+                    transitions.append(Transition(state, next_state, inital_state_probability))
+        for t in terminals:
+            terminal = next(_ for _ in states if _.name == t)
+            transitions.append(Transition(terminal, terminal, 1))
+        return cls(states, transitions)
--- main.py
+++ main.py
@@ -1,69 +1,24 @@
 import numpy as np
 from Markov_Chain import *
+# from state
+
+events = [Event("A",0.4), Event("B", 0.6)]
+goal_states = ["ABA", "BAB"]
+markov_chain = Markov_Chain.get_chain_from_events(goal_states, events)
+markov_chain.draw_transitions()
+
+# markov_process.show_transition_matrix()
+# e = markov_process.get_expected_number_of_steps_to_goal("", "ABBA")
+# print(e)
+# goal_state = "ABAB"
+# markov_process = Markov_Chain.get_chain_from_events(goal_state, events)
+# e = markov_process.get_expected_number_of_steps_to_goal("", "ABAB")
+# print(e)
 
 
-states = [State("", 0, 1),
-          State("A", 1, 0),
-          State("AB", 2, 0),
-          State("ABB", 3, 0),
-          State("ABBA", 4, 0),
-          ]
-
-transitions = [Transition(0, 1, 0.4),
-               Transition(0, 0, 0.6),
-               Transition(1, 1, 0.4),
-               Transition(1, 2, 0.6),
-               Transition(2, 1, 0.4),
-               Transition(2, 3, 0.6),
-               Transition(3, 4, 0.4),
-               Transition(3, 0, 0.6),
-               Transition(4, 4, 1)]
-
-markov_process = Markov_Chain(states, transitions)
-print(markov_process)
-markov_process.show_transition_matrix()
-markov_process.progress_k_steps(50)
-print(markov_process)
-markov_process.reset()
-print(markov_process)
-print(markov_process.expected_number_of_steps_between_states("", "ABBA"))
-
-# Coin Flips
-states = [State("", 0, 1),
-          State("1", 1, 0),
-          State("11", 2, 0),
-          State("111", 3, 0),
-          State("1111", 4, 0),
-          State("11111", 5, 0)
-          ]
-
-transitions = [Transition(0, 1, 0.5),
-               Transition(0, 0, 0.5),
-               Transition(1, 2, 0.5),
-               Transition(1, 0, 0.5),
-               Transition(2, 3, 0.5),
-               Transition(2, 0, 0.5),
-               Transition(3, 4, 0.5),
-               Transition(3, 0, 0.5),
-               Transition(4, 5, 0.5),
-               Transition(4, 0, 0.5),
-               Transition(5, 5, 1)]
-
-markov_process = Markov_Chain(states, transitions)
-print(markov_process)
-markov_process.show_transition_matrix()
-markov_process.progress_k_steps(50)
-print(markov_process)
-markov_process.reset()
-print(markov_process)
-print(markov_process.expected_number_of_steps_between_states("", "11111"))
-
-# state_vector = np.array([0] * 6)
-# state_vector[0] = 1
-# transition_matrix = np.array(
-#     [[1 / 2] + [1 / 2 if j == i else 0 for j in range(5)] for i in range(5)] + [[0, 0, 0, 0, 0, 1]])
-# print(state_vector)
-# print(transition_matrix)
-# for i in range(62):
-#     state_vector = np.dot(state_vector, transition_matrix)
-#     print(state_vector)
+# events = [Event("H", 0.5), Event("T", 0.5)]
+# goal_state = "HHHHH"
+# markov_chain = Markov_Chain.get_chain_from_events(goal_state, events)
+# # markov_chain.draw_transitions()
+# #
+# #
